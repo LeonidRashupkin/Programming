12d1a9e3c6fd3676db609caebc7226d7b6fec6e6
LeonidRashupkin <rashchupkin_leo@mail.ru>
2020-10-30 01:31:38 +0300

    Добавлено 12 задание

---
+++ Practice/12/C++/C++.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    long double a = 1, b = 2, n;
+    cout << "Введите число, факториал которого хотитенайти : ";
+    cin >> n;
+    while (b <= n)
+    { 
+        a = a * b;
+        b++;
+    }
+    cout << "Факториал " << n << " = " << a;
+}
