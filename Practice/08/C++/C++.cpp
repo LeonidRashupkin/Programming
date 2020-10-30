3a1baa11b77cb9706e7f64db2d237a8378dea48a
LeonidRashupkin <rashchupkin_leo@mail.ru>
2020-10-30 20:46:46 +0300

    8 задание

---
+++ Practice/08/C++/C++.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    double a, b, c;
+    char ch;
+    cout << "Введите первое число, символ операции (+ или - или * или /) и второе число через пробел : ";
+    cin >> a >> ch >> b;
+    if (ch == '*')
+    {
+        c = a * b;
+    }
+    else if (ch == '/')
+    {
+        c = a / b;
+    }
+    else if (ch == '+')
+    {
+        c = a + b;
+    }
+    else if (ch == '-')
+    {
+        c = a - b;
+    }
+    else cout << "Неправильный формат ввода";
+    cout << c;
+}
